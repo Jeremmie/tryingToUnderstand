66052421a34b02b34ab31a2b07c7d445685b134c
Jeremmie <jeremie@margouillat.net>
2023-07-23 17:59:46 +0200

    anim ok

--- src/LottieAnimation.jsx
+++ src/LottieAnimation.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { useRef } from "react";
 import Lottie from "lottie-react";
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 import './style.css';
 import testAnim from './assets/test_anim_logo.json';
 
 export default function LottieAnimation(){
-
+    
     return <> 
-    <Lottie className="w-16" animationData={testAnim} loop={false} autoPlay={true}/>;
+        <div>
+        {/* https://github.com/LottieFiles/lottie-react */}
+        <Player
+            onEvent={click => {
+                if (click === 'play') console.log('play'); // check event type and do something
+              }}
+            src={testAnim}
+            style={{ height: '300px', width: '300px' }}
+>
+        <Controls visible={true} buttons={['play', 'frame', 'debug']} />
+        </Player>
+        </div>
     </>
 }
